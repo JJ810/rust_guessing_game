4598e47b6f99f4f0e9782ba1f43ad46f58b97a31
JJ810 <hcls80205@gmail.com>
2024-10-10 11:28:32 -0400

    Use if, match, loop expressions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
-    println!("Hey, guess a number:");
-    let mut num = String::new();
+    let correct = rand::thread_rng().gen_range(1..=10);
+    println!("The correct number is: {}", correct);
+    println!("Hey, guess a number 1-10:");
 
-    io::stdin()
-        .read_line(&mut num)
-        .expect("Error reading input");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading input");
 
-    println!("You guessed: {}", num.trim());
+        let guess: u32 = guess.trim().parse().expect("Error parsing guess.");
+
+        // if expression
+        // let mut message = if correct < guess {
+        //     String::from("You guessed too high.")
+        // } else if correct > guess {
+        //     String::from("You guessed too low.")
+        // } else {
+        //     String::from("You guessed the correct number.")
+        // };
+
+        // match expression
+        let message = match guess.cmp(&correct) {
+            Ordering::Greater => "You guessed too high.",
+            Ordering::Less => "You guessed too low.",
+            Ordering::Equal => {
+                println!("You guessed the correct number.");
+                break;
+            }
+        };
+
+        println!("{message}");
+    }
 }
